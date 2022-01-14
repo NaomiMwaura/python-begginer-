c80d7169d1c808ee484c2d0f76509661936a0809
NaomiMwaura <mwauranaomi2740@gmail.com>
2022-01-14 22:38:42 +0300

    my second commit

---
+++ python data types.py
@@ -0,0 +1,28 @@
+a_str = 'hello world'
+print (a_str)
+print(a_str [0])
+print(a_str[0:5])
+
+b_str = "make me a pro"
+print(b_str)
+print(b_str[1])
+print("i'll be a \npro programmer")
+
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
+a = set('abracadabra')
+print(a)
+a.add('z')
+print(a)
+
+a = frozenset ('abracadabra')
+print(a)
+where = frozenset(['nairobi', 'nakuru', 'LA'])
+print(where)
+
+print('3.14')
+
+dic = {'name':'turquoise','base':12}
+print(dic)
+print(dic['base'])
+print(dic.values())
